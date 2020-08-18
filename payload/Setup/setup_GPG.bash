331e8b3ae2dad3d93c55a5fac1ba66fb70cded68
reactive-firewall <reactive-firewall@users.noreply.github.com>
2020-08-18 12:12:52 -0700

    more setup stuff

--- payload/Setup/setup_GPG.bash
+++ payload/Setup/setup_GPG.bash
@@ -1,16 +1,22 @@
 #! /bin/bash
-#softwareupdate --install --recommended
+
+if [[ ( -z $(command -v gpg2 ) ) ]] ; then
+if [[ $( \uname -s ) == "Darwin" ]] ; then
+sudo softwareupdate --install --recommended || true
 #curl -L --url https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-1.1.5-mac.pkg
 #sudo installer -store -pkg /Volumes/GPG_Suite/Install.pkg -target / ; wait ; sync ; wait ;
-
+bash -c $(dirname $0)/setup_homebrew.bash
 
 # this takes some time:
-curl -fsSL --tlsv1.2 --url https://releases.gpgtools.org/GPG_Suite-2020.1.dmg --out /tmp/GPG_Suite.dmg
+curl -fsSL --header "Dnt: 1" --tlsv1.2 --url https://releases.gpgtools.org/GPG_Suite-2020.1.dmg --out /tmp/GPG_Suite.dmg
 hdiutil attach /tmp/GPG_Suite.dmg -mountPoint /Volumes/GPG_Suite
 # must be admin user to install:
 sudo installer -store -pkg /Volumes/GPG_Suite/Install.pkg -target /
 wait ; sync ; wait ;
 hdiutil detach /Volumes/GPG_Suite || hdiutil detach /Volumes/GPG_Suite -force ; wait ;
+
+fi
+fi
 
 # ensure the config files are created (done automatically by gpg2 when first run)
 gpg2 --list-keys ;
@@ -37,12 +43,18 @@ echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf ;
 echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
 
+if [[ $( \uname -s ) == "Darwin" ]] ; then
+if [[ ( -z $( grep -x -F "scdeamon-program" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
+echo "scdeamon-program /usr/local/MacGPG2/libexec/scdeamon" >> ~/.gnupg/gpg-agent.conf ;
+echo "" >> ~/.gnupg/gpg-agent.conf ;
+fi
+if [[ ( -z $( grep -F "pinentry-program " ~/.gnupg/gpg-agent.conf ) ) ]] ; then
+echo "pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac" >> ~/.gnupg/gpg-agent.conf ;
+echo "" >> ~/.gnupg/gpg-agent.conf ;
+fi
+else
 if [[ ( -z $( grep -x -F "write-env-file" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
 echo "write-env-file" >> ~/.gnupg/gpg-agent.conf ;
 echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
-
-if [[ ( -z $( grep -F "pinentry-program " ~/.gnupg/gpg-agent.conf ) ) ]] ; then
-echo "pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac" >> ~/.gnupg/gpg-agent.conf ;
-echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
