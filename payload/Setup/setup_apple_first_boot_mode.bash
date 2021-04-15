1eaf76a7cdd0b1b1a909b64dd1c60748d8a353c8
reactive-firewall <reactive-firewall@users.noreply.github.com>
2021-04-15 16:47:45 -0700

    More Env more startup scripts more is less

--- payload/Setup/setup_apple_first_boot_mode.bash
+++ payload/Setup/setup_apple_first_boot_mode.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-rm /var/db/.applesetupdone || true
+rm -qf /var/db/.applesetupdone || true
+(find / -type f -iname ".DS_Store" -print0 2>/dev/null | xargs -0 -L1 -I{} rm -qf "{}" 2>/dev/null ) || true
 
 exit 0
