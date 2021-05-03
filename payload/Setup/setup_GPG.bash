bc7f22cffa512ce874f385b11e034e9d081eaac4
reactive-firewall <reactive-firewall@users.noreply.github.com>
2021-05-03 13:29:14 -0700

    revert regression bug on GPG setup script

--- payload/Setup/setup_GPG.bash
+++ payload/Setup/setup_GPG.bash
@@ -24,33 +24,33 @@ hash -p /usr/local/MacGPG2/bin/gpg2 gpg2
 gpg2 --list-keys ;
 
 #prefer SHA512 instead of SHA1
-if [[ ( -z $( grep -v -F "personal-digest-preferences SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
+if [[ ( -z $( grep -F "personal-digest-preferences SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
 echo "personal-digest-preferences SHA512 SHA384 SHA256 SHA224" >> ~/.gnupg/gpg.conf ;
 fi
-if [[ ( -z $( grep -v -F "cert-digest-algo SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
+if [[ ( -z $( grep -F "cert-digest-algo SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
 echo "cert-digest-algo SHA512" >> ~/.gnupg/gpg.conf ;
 echo "" >> ~/.gnupg/gpg.conf ;
 fi
-if [[ ( -z $( grep -v -F "default-preference-list SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
+if [[ ( -z $( grep -F "default-preference-list SHA512" ~/.gnupg/gpg.conf ) ) ]] ; then
 echo "default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES BZIP2 ZLIB ZIP Uncompressed" >> ~/.gnupg/gpg.conf ;
 echo "" >> ~/.gnupg/gpg.conf ;
 fi
-if [[ ( -z $( grep -x -v -F "use-agent" ~/.gnupg/gpg.conf ) ) ]] ; then
+if [[ ( -z $( grep -x -F "use-agent" ~/.gnupg/gpg.conf ) ) ]] ; then
 echo "use-agent" >> ~/.gnupg/gpg.conf ;
 echo "" >> ~/.gnupg/gpg.conf ;
 fi
 
-if [[ ( -z $( grep -x -v -F "enable-ssh-support" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
+if [[ ( -z $( grep -x -F "enable-ssh-support" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
 echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf ;
 echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
 
 if [[ $( \uname -s ) == "Darwin" ]] ; then
-if [[ ( -z $( grep -v -F "scdaemon-program" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
+if [[ ( -z $( grep -F "scdaemon-program" ~/.gnupg/gpg-agent.conf ) ) ]] ; then
 echo "scdaemon-program /usr/local/MacGPG2/libexec/scdaemon" >> ~/.gnupg/gpg-agent.conf ;
 echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
-if [[ ( -z $( grep -v -F "pinentry-program " ~/.gnupg/gpg-agent.conf ) ) ]] ; then
+if [[ ( -z $( grep -F "pinentry-program " ~/.gnupg/gpg-agent.conf ) ) ]] ; then
 echo "pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac" >> ~/.gnupg/gpg-agent.conf ;
 echo "" >> ~/.gnupg/gpg-agent.conf ;
 fi
