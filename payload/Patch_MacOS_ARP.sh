0a7bff9488e30477174ccba310f6c1fa5bc3cee6
reactive-firewall <reactive-firewall@users.noreply.github.com>
2021-10-07 23:31:04 -0700

    [HOTFIX-5] Fix for forced RFC-compliance

--- payload/Patch_MacOS_ARP.sh
+++ payload/Patch_MacOS_ARP.sh
@@ -2,22 +2,24 @@
 #############################################
 # AUTHOR: REACTIVE-FIREWALL                 #
 # VERSION 1.00 RELEASE DATE OCT 15 2020     #
-# DESC:  FORCE rfc4193 PREFIX ONLY          #
-# REQUIREMENTS:                             #
-#  OS X 10.15 or newer                      #
+# DESC:  Disable BRAIN-DEAD MacOS ARP Cache #
 #############################################
+#REQUIREMENTS:
+#  OS X 10.15 or newer
+#############################################
+#CHECK FOR OS X 10.15
 if [[  $(sw_vers -productVersion | grep '10.15')  ]] || [[  $(sw_vers -productVersion | grep '11.')  ]]
 then
 	if [[ -f /etc/sysctl.conf ]]
 	then
-		if grep 'rfc4193' /etc/sysctl.conf > /dev/null 2>&1
+		if grep 'unicast' /etc/sysctl.conf > /dev/null 2>&1
 		then
-		echo "PATCH WAS PREVIOUSLY ENABLED"
-		exit
+			echo "PATCH WAS PREVIOUSLY ENABLED"
+			exit 0;
 		fi
 	fi
-	sudo sysctl -w net.inet6.ip6.only_allow_rfc4193_prefixes=1 > /dev/null 2>&1
-	echo "net.inet6.ip6.only_allow_rfc4193_prefixes=1" | sudo tee -a /etc/sysctl.conf > /dev/null 2>&1
+	sudo sysctl -w net.link.ether.inet.arp_unicast_lim=0  > /dev/null 2>&1
+	echo "net.link.ether.inet.arp_unicast_lim=0" | sudo tee -a /etc/sysctl.conf  > /dev/null 2>&1
 	sudo chown root:wheel /etc/sysctl.conf
 	sudo chmod 644 /etc/sysctl.conf
 	echo "PATCH ENABLED"
